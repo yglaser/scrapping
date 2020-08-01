06ec54aea4ac4b32e87704fc14482d5a25fd5902
yanina <yglaser@mobilecomputing.com.ar>
2020-08-01 16:31:11 -0300

    agregado add

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const CronJob = require('cron').CronJob;
+const scrapping = require('./scrapping-megatlon')
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), () => {
+    const job = new CronJob('0 0 * * 1,3-6', function() {
+        const d = new Date();
+      console.log('At midnigth', d);
+      scrapping.scrapping()
+    });
+    console.log('After job instantiation');
+    job.start();
+    console.log("running on port" )
+})
